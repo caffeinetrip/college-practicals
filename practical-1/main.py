51c3be8664018c99039161ac366979254f33adf1
caffeinetrip <shizenking@gmail.com>
2025-05-24 17:29:47 +0300

    add: 1 practical complete, repo initialized

---
+++ practical-1/main.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+class Student:
+    def __init__(self, name=None, surname=None, year=None):
+        self.name = name
+        self.surname = surname
+        self.year = year
+
+    @property
+    def list(self):
+        return [self.name, self.surname]
+
+    @property
+    def course(self):
+        if self.year is None:
+            return None
+
+        current_year = datetime.now().year
+        age = current_year - self.year
+
+        if age <= 15:
+            return 'too young'
+
+        return min((age - 16) + 1, 4)
+
+pavluk_vlad = Student('Vlad', 'Pavluk', 2008)
+print(pavluk_vlad.course)
+print(pavluk_vlad.list)
